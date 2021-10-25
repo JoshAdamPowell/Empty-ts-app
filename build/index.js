34cc547961f4bff09ec45e53385786c58f260ad0
Josh Powell <josh.powell@softwire.com>
2021-10-25 14:44:01 +0100

    Empty TS app

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const App = {
+    start: () => {
+        console.log('hello world');
+    }
+};
+App.start();
+exports.default = App;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const App = {
+    start: () => {
+        console.log('hello world');
+    }
+}
+
+
+App.start();
+
+export default App;
