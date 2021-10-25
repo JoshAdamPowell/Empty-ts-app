db3e953f2065b8257f3e004ff2851d3e8829b6d9
Josh Powell <josh.powell@softwire.com>
2021-10-25 14:47:05 +0100

    Remove build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App = {
-    start: () => {
-        console.log('hello world');
-    }
-};
-App.start();
-exports.default = App;
